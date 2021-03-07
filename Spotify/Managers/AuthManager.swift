48c4bfc9cf61f5fd3b64436b742d180f324e53f6
Sa3doola <saadsherif02@gmail.com>
2021-03-07 22:30:51 +0200

    Before API Browser

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -122,12 +122,12 @@ final class AuthManager {
         }
     }
     
-    public func refreshIfNeeded(completion: @escaping (Bool) -> Void) {
+    public func refreshIfNeeded(completion: ((Bool) -> Void)? ) {
         guard !refreshingToken else {
             return
         }
         guard shouldRefreshToken else {
-            completion(true)
+            completion?(true)
             return
         }
         
@@ -156,7 +156,7 @@ final class AuthManager {
         let data = basicToken.data(using: .utf8)
         guard let base64String = data?.base64EncodedString() else {
             print("Failure to get base64")
-            completion(false)
+            completion?(false)
             return
         }
         request.setValue("Basic \(base64String)", forHTTPHeaderField: "Authorization")
@@ -164,7 +164,7 @@ final class AuthManager {
         let task = URLSession.shared.dataTask(with: request) { [weak self] (data, _, error) in
             self?.refreshingToken = false
             guard let data = data, error == nil else {
-                completion(false)
+                completion?(false)
                 return
             }
             
@@ -173,11 +173,11 @@ final class AuthManager {
                 self?.onRefreshBlock.forEach { $0(result.access_token!) }
                 self?.onRefreshBlock.removeAll()
                 self?.cacheToken(result: result)
-                completion(true)
+                completion?(true)
             }
             catch {
                 print(error.localizedDescription)
-                completion(false)
+                completion?(false)
             }
             
         }
